25b15c7f67864bd46629b36e2b25705e8358c2ce
PedroGomes02 <pmpgomes@gmail.com>
2022-12-15 11:26:05 +0000

    Typescript Portal Container Fix

--- src/components/Backdrop.tsx
+++ src/components/Backdrop.tsx
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "../App.css"
 
-export default function Backdrop (props: { children: any; closeModal: any }) {
-const {children, closeModal} = props
+interface PortalProps {
+    children: any,
+    container: HTMLElement,
+    closeModal: any
+}
+
+export default function Backdrop (props: PortalProps) {
+const {children, container, closeModal} = props
     return ReactDOM.createPortal(
         <div className="backdrop" onClick={closeModal}>{children}</div>,
-        document.getElementById("portal"), null
+        container
     )
 }
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -5,7 +5,7 @@ import "../App.css"
 export default function Modal (props: {closeModal: any }) {
 const {closeModal} = props
     return (
-        <Backdrop closeModal={closeModal}>
+        <Backdrop closeModal={closeModal} container={document.getElementById("portal") as HTMLElement}>
         <div className="modal" onClick={(e)=> e.stopPropagation()}>
             <h3>Modal</h3>
             <p>This is a test modal component with React Portal</p>
